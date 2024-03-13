16b93c5131f801fbebd84ffa8be958d22562d190
MehjabinKhan <161743992+MehjabinKhan@users.noreply.github.com>
2024-03-13 23:27:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var roleModelName = "Muhammad Ali Jinnah";
+var dateOfBirth = "25 of december 1876";
+var title = "Quaid-e-Azam";
+var strugglePeriod = "Work hard for the creation of Pakistan";
+var personality = "smart lawyer and a good leader";
+var hisFamousQuote = "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.";
+console.log("My role model name is :".concat(roleModelName, ". He was born on :").concat(dateOfBirth, ". Jinnah was known as the :").concat(title, "."));
+console.log("He :".concat(strugglePeriod, ". Quaid-e-Azam was a :").concat(personality, ". and Quaid famous quote is :").concat(hisFamousQuote, "."));
---
+++ main.ts
@@ -0,0 +1,9 @@
+let roleModelName = "Muhammad Ali Jinnah";
+let dateOfBirth = "25 of december 1876";
+let title = "Quaid-e-Azam";
+let strugglePeriod = "Work hard for the creation of Pakistan";
+let personality = "smart lawyer and a good leader";
+let hisFamousQuote = "Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man.";
+
+console.log(`My role model name is :${roleModelName}. He was born on :${dateOfBirth}. Jinnah was known as the :${title}.`);
+console.log(`He :${strugglePeriod}. Quaid-e-Azam was a :${personality}. and Quaid famous quote is :${hisFamousQuote}.`)
